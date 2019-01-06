47e606bab32fb05b8f12c3c2108a9d9fdcc55650
SingularGamesStudio <40793299+SingularGamesStudio@users.noreply.github.com>
2019-01-06 10:39:23 +0300

    +++++

---
+++ havchik_ащквшфд2/Assets/scripts/anysoundlevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anysoundlevel : MonoBehaviour {
+	public bool ismusic;
+	public int levelset;
+	public Sprite sp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnMouseDown(){
+		if (ismusic) {
+			main._m.musiclevelset (levelset);
+		} else {
+			main._m.soundlevelset (levelset);
+		}
+	}
+}
