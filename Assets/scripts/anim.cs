4fc079593ff05c23573145ac9ffb814a192fcf2a
SingularGamesStudio <40793299+SingularGamesStudio@users.noreply.github.com>
2020-05-19 10:01:55 +0300

    Init commit

---
+++ Assets/scripts/anim.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class anim : MonoBehaviour {
+    public Animation a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!a.isPlaying) {
+			a.Play ();
+		}
+	}
+}
---
+++ Assets/scripts/maincameracontroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maincameracontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(main._m.ingame)
+		gameObject.transform.position = new Vector3(main._m.teams[0].comgo.transform.position.x, main._m.teams[0].comgo.transform.position.y, -10);
+    }
+}
